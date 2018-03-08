89a799319274b02f42d5c27c79562e0c7a5697c2
Luke Fleming <117474182@umail.ucc.ie>
2018-03-08 19:50:07 +0000

    update

---
+++ Ebus3.php
@@ -0,0 +1,103 @@
+<?php
+// Start the session
+session_start()
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Receipt</title>
+        
+        <!--Stylesheets -->
+        <link rel="stylesheet" href="shop_navbar.css" type="text/css"/>
+        <link rel="stylesheet" href="Ebus.css" type="text/css"/>
+        
+        <!-- Setting the webpage font -->
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        
+        <!-- setting webpage favicon -->
+        <link rel="shortcut icon" type="image/ico" href="Images/favicon.ico">
+        
+        <!-- Google icons -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    </head>
+    
+    <body>
+        <!-- Adding navigation bar to the page -->
+        <header>
+            <div class="container">
+                
+                <img class="logo" src="../Images/company_logo_trans.png">
+                
+                <nav>
+                    <ul>
+                        <li><a href="shophome.html">Shop Home</a></li>
+                        <li><a href="about_cloud.html">About Cloud</a></li>
+                        <li><a href="products.html">Products</a></li>
+                        <li><a href="../homepage.html">Personal Website</a></li>
+                    </ul>
+                </nav>
+                
+            </div>
+        </header>
+        
+        <!-- Adding content to the page -->
+        <div class="ebus_content">
+          <center>  <h1 class="ebus3_heading">Purchase Receipt</h1> 
+            
+            <?php  
+            // Set session variables
+              $_SESSION["user_name"] = $_POST["user_name"];
+              $_SESSION["user_email"] = $_POST["user_email"];
+            ?>
+            
+            <p class="ebus3_name">
+                <?php
+                // Echo session variables that were set on the previous pages
+                echo "Username: " . $_SESSION["user_name"] . ".";
+                ?>
+            </p>
+            
+            <p class="ebus3_email">
+                <?php
+                // Echo session variables that were set on the previous pages
+                echo "Email: " . $_SESSION["user_email"] . ".";
+                ?>
+            </p>
+            
+            <p class="ebus3_total">
+                <?php
+                // Echo session variables that were set on the previous pages
+                echo "Total: " . $_SESSION["total"] . ".";
+                ?> </center>
+            </p>
+            
+            <br>
+            
+            <h3 class="ebus3_heading2">Thank you for your purchase!</h3>
+            
+            <br>
+            
+            <p class="ebus3_message">
+               Cloudhub customer service team will be in contact regarding your purchase
+                
+               
+                
+                
+            </p>
+            
+            <a class="home_btn" role="button" href="shophome.html">Home</a>
+        </div>
+        
+        <!-- Displaying information of ficticous company-->
+        <footer class="ebus_copyright">
+            <p>
+                <small>&copy;Cloudhub is a ficticous company.</small>
+                <br>
+                
+            </p>
+        </footer>
+    </body>
+</html>
